4a0396ccf893140efbb903ef06e821c636fe0a15
Merluciusz <limestoragesolutions@gmail.com>
2022-04-06 15:18:59 +0300

    Adding the first exercise

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WorkSpace wSpace = new WorkSpace();
+        System.out.println(wSpace.isAnagram("lime","emil"));
+    }
+}
